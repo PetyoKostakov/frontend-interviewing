1d2d27b2f8d47604fa8ff8de269f7156956a4dc1
Petyo Kostakov <petyo_kostakov@starsgroup.com>
2020-03-30 18:48:37 +0300

    removeDuplicates implementation

--- questions/duplicateStrings.js
+++ questions/duplicateStrings.js
@@ -5,3 +5,28 @@
  const str = 'This is is a test test string';
  removeDuplicates(str); // 'This is a test string'
  */
+
+// function removeDuplicates(str) {
+//     return Object.keys(str.split(' ').reduce((acc, cur) => {
+//         acc[cur] = cur;
+//
+//         return acc;
+//     }, {})).join(' ');
+// }
+
+console.log(removeDuplicates('This is is a test test string '));
+
+// function removeDuplicates(str) {
+//     return str.split(' ').reduce((acc, currentWord) => {
+//        if (!acc.indexOf(currentWord) > -1) {
+//            acc.push(currentWord);
+//        }
+//
+//        return acc;
+//     }, []).join(' ');
+// }
+
+
+function removeDuplicates(str) {
+    return [...new Set(str.split(' '))].join(' ');
+}
