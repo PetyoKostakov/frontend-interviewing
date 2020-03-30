fce85a5e24d395d76d01f6e69426a67199903c00
PetyoKostakov <PetyoKostakov@gmail.com>
2020-03-30 18:07:20 +0300

    Reverse question implementation

--- questions/reverse.js
+++ questions/reverse.js
@@ -3,3 +3,39 @@
 /*
     reverse('Hello, world!'); // !dlrow ,olleH
 */
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+should(
+    'Should reverse "abc" return "cba" -',
+    reverse,
+    'abc',
+    'cba'
+);
+
+should(
+    'Should reverse "" empty string',
+    reverse,
+    '',
+    ''
+);
+
+should(
+    'Should reverse "Longer than expected text!"',
+    reverse,
+    "Longer than expected text!",
+    "!txet detcepxe naht regnoL"
+);
+
+function should(testTitle, functionToTest, arguments, expectedResult) {
+    console.log(testTitle);
+    const result = functionToTest(arguments);
+
+    if (result === expectedResult) {
+        console.log('pass');
+    } else {
+        console.log('fail');
+    }
+}
